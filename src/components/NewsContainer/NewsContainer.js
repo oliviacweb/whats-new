3f126329fc640f1509faa316ec19234222a871fb
Olivia Webster <oliviacweb@gmail.com>
2020-05-06 21:01:11 -0400

    Create container for local news articles

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import './NewsContainer.css'
+import './NewsContainer.css';
+import NewsArticle from '../NewsArticle/NewsArticle.js';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+
+const NewsContainer = (props) => {
+  const localNews = props.news.map(article => {
+    return <NewsArticle
+      key = {article.id}
+      headline = {article.headline}
+      img = {article.img}
+      description = {article.description}
+      url = {article.url}
+    />
+  })
+  return (
+    <section className='news-container'>
+      {localNews}
+    </section>
+  )
+}
+
+
 
 export default NewsContainer;
