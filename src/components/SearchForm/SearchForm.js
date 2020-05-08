4fa120018357be2a41b58ec730bcced719b1a8a6
Olivia Webster <oliviacweb@gmail.com>
2020-05-07 20:14:38 -0400

    Create search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      search: ''
+    }
+  }
+
+  createTerm = (event) => {
+    this.setState({ search: event.target.value })
+    console.log(this.state.search);
+  }
+
+  render () {
+    return (
+      <form className="search-form">
+        <input
+        type="text"
+        className="search-field"
+        name="searchField"
+        placeholder="search for articles"
+        onChange={ (event) => this.createTerm(event) }
+        />
+        <button className="search-btn" onClick={ (event) => this.props.filterArticles(event, this.state.search) }>find</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
